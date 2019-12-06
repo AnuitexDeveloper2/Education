32654716f800022affaf82059aced1bd9ae497f2
Volodymyr Honcharuk <morgenshtern1988@gmail.com>
2019-12-06 12:58:06 +0200

    Add ApiModules

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AccountModule} from '../app/account/account.module';
+import { AdministratorModule } from './administrator/administrator.module';
+import { AuthorModule } from './author/author.module';
+import { OrderModule } from './order/order.module';
+import { PrintingEditionModule } from './printing-edition/printing-edition.module';
+import { SharedModule } from './shared/shared.module';
+import { UserModule } from './user/user.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AccountModule,
+    FormsModule,
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AdministratorModule,
+    AuthorModule,
+    OrderModule,
+    PrintingEditionModule,
+    SharedModule,
+    UserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
